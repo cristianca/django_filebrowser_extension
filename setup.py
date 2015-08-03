946dcbab8ba567fcb720fdf23cbe6807be773a48
tomasz.roszko <tomaszroszko@gmail.com>
2015-08-03 14:27:21 +0200

    Create setup file, authors and manifest

---
+++ setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='django_filebrowser_extension',
+    version='0.0.1',
+    author='Tomasz Roszko',
+    author_email='tomaszroszko@tjsoftware.co',
+    description='Extension for django-filebrowser',
+    long_description=open('README.md').read(),
+    url='https://github.com/tomaszroszko/django_filebrowser_extension',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
