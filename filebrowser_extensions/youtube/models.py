bb563b281abb4a14f344d761150d6b2509bdea5a
tomasz.roszko <tomaszroszko@gmail.com>
2015-11-06 11:04:22 +0100

    Saving youtube thumbnail during youtube save

--- filebrowser_extensions/youtube/models.py
+++ filebrowser_extensions/youtube/models.py
@@ -1,6 +1,12 @@
+import os
 from django.utils.translation import ugettext_lazy as _
 from embed_video.backends import YoutubeBackend
+import requests
 from filebrowser_extensions.iframes.models import IFrameAbstract
+from filebrowser.settings import DIRECTORY, MEDIA_ROOT, MEDIA_URL
+
+
+YOUTUBE_THUMBNAIL_DIRECTORY = 'youtube_thumbnails'
 
 
 class Youtube(IFrameAbstract):
@@ -11,6 +17,31 @@ class Youtube(IFrameAbstract):
     class Meta:
         verbose_name = _('Youtube')
         verbose_name_plural = _('Youtubes')
+
+    def download_youtube_thumbnail(self):
+        """
+        download youtube thumbnail and save it in uploads folder 'youtube_thumbnails'
+        :return: path to to youtube thumbnail
+        """
+
+        file_name = self.youtube_thumbnail.split('/')[-1]
+        path = os.path.join(MEDIA_ROOT, DIRECTORY, YOUTUBE_THUMBNAIL_DIRECTORY, file_name)
+
+        r = requests.get(self.youtube_thumbnail, stream=True)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+        return os.path.join(DIRECTORY, YOUTUBE_THUMBNAIL_DIRECTORY, file_name)
+
+    def save(self, *args, **kwargs):
+        """
+        save object and if thumbnail is not given, get original thumbnail
+        from youtube
+        """
+        if not self.thumbnail:
+            self.thumbnail = self.download_youtube_thumbnail()
+        super(Youtube, self).save(*args, **kwargs)
 
     @property
     def youtube_backend(self):
@@ -30,10 +61,8 @@ class Youtube(IFrameAbstract):
         return self.youtube_backend.url
 
     @property
-    def thumbnail(self):
+    def youtube_thumbnail(self):
         """
         return youtube thumbnail
         """
         return self.youtube_backend.thumbnail
-
-
