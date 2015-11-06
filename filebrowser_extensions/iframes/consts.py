13e4d20035751ed19148895500c69475f1a6aef4
tomasz.roszko <tomaszroszko@gmail.com>
2015-11-06 13:45:51 +0100

    Make thumbnails saved for vimeo and daily montion iframes. This definitly to to be rewrited, as currently it's very quick and dirty implementation

---
+++ filebrowser_extensions/iframes/consts.py
@@ -0,0 +1,9 @@
+IFRAME_VIMEO = 1
+IFRAME_DAILYMOTION = 2
+
+# detection based on url in iframe
+IFRAMES_DETECTION = {
+    IFRAME_VIMEO: 'player.vimeo.com',
+    IFRAME_DAILYMOTION: 'www.dailymotion.com',
+}
+
--- filebrowser_extensions/iframes/models.py
+++ filebrowser_extensions/iframes/models.py
@@ -1,10 +1,15 @@
+import os
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 from bs4 import BeautifulSoup
+from embed_video.backends import VimeoBackend
 from filebrowser.fields import FileBrowseField
+from filebrowser.settings import MEDIA_ROOT, DIRECTORY
+import requests
+from .consts import IFRAMES_DETECTION, IFRAME_VIMEO, IFRAME_DAILYMOTION
 
 
 class IFrameAbstract(models.Model):
@@ -63,8 +68,60 @@ class IFrame(IFrameAbstract):
         on :param:`code` but b'coz we can get different width and height, we need
         to edit that
         """
-        soup = BeautifulSoup(self.code)
-        iframe = soup.find('iframe')
+        iframe = self.iframe_soup
         iframe['width'] = width
         iframe['height'] = height
         return mark_safe(iframe.prettify())
+
+    @property
+    def iframe_soup(self):
+        """
+        :return: bs4 iframe object
+        """
+        if not hasattr(self, '__iframe_soup__'):
+            soup = BeautifulSoup(self.code)
+            self.__iframe_soup__ = soup.find('iframe')
+        return self.__iframe_soup__
+
+    @property
+    def iframe_src(self):
+        return self.iframe_soup['src']
+
+    @property
+    def iframe_type(self):
+        for iframe_type, iframe_url in IFRAMES_DETECTION.items():
+            if iframe_url in self.iframe_src:
+                return iframe_type
+        return None
+
+    def download_thumbnail(self, thumbnail, file_name, directory):
+        # TODO: this solution is temporary, need to rewrite that
+        path = os.path.join(MEDIA_ROOT, DIRECTORY, directory, file_name)
+        r = requests.get(thumbnail, stream=True)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+        return os.path.join(DIRECTORY, directory, file_name)
+
+    def save(self, *args, **kwargs):
+        """
+        Save and check if we can recognize what type of iframe it is (from what service)
+        so we can for example add thumbnail automaticly
+        """
+
+        # TODO: make it nice easy to extend and so on...
+        if not self.thumbnail:
+            t = self.iframe_type
+            if t == IFRAME_VIMEO:
+                vimeo = VimeoBackend(self.code)
+                thumbnail = vimeo.get_thumbnail_url()
+                file_name = thumbnail.split('/')[-1]
+                self.thumbnail = self.download_thumbnail(thumbnail, file_name, 'vimeo_thumbnails')
+            elif t == IFRAME_DAILYMOTION:
+                thumbnail = self.iframe_src.replace('embed', 'thumbnail')
+                file_name = thumbnail.split('/')[-1] + '.jpg'
+                self.thumbnail = self.download_thumbnail(thumbnail, file_name, 'dailymontion_thumbnails')
+
+
+        super(IFrame, self).save(*args, **kwargs)
